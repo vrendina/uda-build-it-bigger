e9d1046b582fbc03e339ddd57b69a28f13ffc743
Victor Rendina <vrendina@gmail.com>
2017-04-26 13:21:13 -0400

    AsyncTaskLoader to load joke data

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -4,34 +4,64 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.udacity.gradle.joketeller.Joke;
 import com.udacity.gradle.joketeller.JokeTeller;
 
 import timber.log.Timber;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Joke>{
 
-    private JokeTeller jokeTeller;
+    public static final int TASK_LOCAL_ID = 100;
+    public static final int TASK_REMOTE_ID = 200;
+
+    public static final String LAST_JOKE_KEY = "lastJoke";
+
+    private TextView loadingTextView;
+    private ProgressBar loadingProgressBar;
+    private Button tellJokeButton;
+
+    private Joke lastJoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize the local joke teller
-        jokeTeller = new JokeTeller();
+        loadingTextView = (TextView) findViewById(R.id.tv_loading_joke);
+        loadingProgressBar = (ProgressBar) findViewById(R.id.pb_loading_joke);
+        tellJokeButton = (Button) findViewById(R.id.btn_tell_joke);
+
+        getSupportLoaderManager().initLoader(TASK_LOCAL_ID, null, this);
+        getSupportLoaderManager().initLoader(TASK_REMOTE_ID, null, this);
+
+        if(savedInstanceState != null) {
+            lastJoke = (Joke) savedInstanceState.getSerializable(LAST_JOKE_KEY);
+        }
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Save the last joke we told so we can always get fresh material!
+        if(lastJoke != null) {
+            outState.putSerializable(LAST_JOKE_KEY, lastJoke);
+        }
+        super.onSaveInstanceState(outState);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -41,31 +71,105 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
-            Timber.d("Loading settings activity...");
-
             Intent settingsIntent = new Intent(this, SettingsActivity.class);
             startActivity(settingsIntent);
-
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void toggleLoadingIndicator(boolean show) {
+        if(show) {
+            tellJokeButton.setEnabled(false);
+            loadingTextView.setVisibility(View.VISIBLE);
+            loadingProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            tellJokeButton.setEnabled(true);
+            loadingTextView.setVisibility(View.GONE);
+            loadingProgressBar.setVisibility(View.GONE);
+        }
+    }
+
     public void tellJoke(View view) {
+        toggleLoadingIndicator(true);
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String jokeSourceKey = prefs.getString(getString(R.string.sp_key_joke_source),
                 getString(R.string.default_joke_source));
 
+        int taskId = 0;
         if(jokeSourceKey.equals(getString(R.string.key_joke_source_local))) {
-            Timber.d("Telling joke from local java library...");
+            taskId = TASK_LOCAL_ID;
         } else if(jokeSourceKey.equals(getString(R.string.key_joke_source_gce))) {
-            Timber.d("Telling joke from remote gce source...");
+            taskId = TASK_REMOTE_ID;
         }
 
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        LoaderManager loaderManager = getSupportLoaderManager();
+        loaderManager.restartLoader(taskId, null, this);
+        loaderManager.getLoader(taskId).forceLoad();
+    }
+
+    private Joke loadLocalData() {
+        // Artificially inflate the time it takes to get a local joke
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        JokeTeller teller = new JokeTeller();
+        return teller.tellJoke();
     }
 
 
+    @Override
+    public Loader<Joke> onCreateLoader(final int id, Bundle args) {
+        return new AsyncTaskLoader<Joke>(this) {
+            @Override
+            public Joke loadInBackground() {
+
+                Joke joke = null;
+
+                // How many times we will try to get a fresh joke if we get the same one
+                int retryCount = 0;
+                int retryLimit = 3;
+
+                switch (id) {
+                    case TASK_LOCAL_ID:
+                        Timber.d("Telling joke from local java library...");
+                        joke = loadLocalData();
+
+                        while(joke == lastJoke && retryCount < retryLimit) {
+                            Timber.d("Got the same joke, looking for fresh material!");
+                            joke = loadLocalData();
+                            retryCount++;
+                        }
+
+                        break;
+
+                    case TASK_REMOTE_ID:
+                        Timber.d("Telling joke from remote gce source...");
+                        joke = new Joke("test", "remote");
+
+                        break;
+                }
+
+                return joke;
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Joke> loader, Joke data) {
+        Timber.d("Got joke data -- " + data.toString());
+
+        lastJoke = data;
+        toggleLoadingIndicator(false);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Joke> loader) {
+
+    }
 }
